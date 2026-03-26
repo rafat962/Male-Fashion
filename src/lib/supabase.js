e5f067206db1dc188c05998d4feb2ee4a997ff49
rafat962 <rafatkamel96@gmail.com>
2026-03-26 10:27:11 +0200

    main

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,7 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-console.log("URL Check:", import.meta.env.VITE_SUPABASE_URL);
 if (!SUPABASE_URL || !SUPABASE_KEY) {
     throw new Error("Missing Supabase environment variables");
 }
